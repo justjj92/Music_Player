14d4d73567ca943aa854ce546845028c657491ac
justjj92 <justin.jordan92@gmail.com>
2023-01-30 18:39:43 -0800

    Initial Commit

---
+++ Music_Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music_Player
+//
+//  Created by J J on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
